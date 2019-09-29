1d6ee29fa42790cb5124f74b90605aff4e7b8536
vardaro <anthonyavardaro@yahoo.com>
2019-09-29 01:25:15 -0400

    add wasm

--- src/components/SimulationWrapper/SimulationWrapper.js
+++ src/components/SimulationWrapper/SimulationWrapper.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import SimulationListItem from '../SimulationListItem/SimulationListItem';
-const endpoint = "https://relativity-server.herokuapp.com/api/simulations";
+// const endpoint = "https://relativity-server.herokuapp.com/api/simulations";
+const endpoint = "http://localhost:42/api/simulations"
+const wasmEndpoint = "localhost:42/static/build.wasm";
+
 
 class SimulationWrapper extends React.Component {
     constructor(props) {
@@ -9,7 +12,7 @@ class SimulationWrapper extends React.Component {
 
         this.state = {
             sims: [],
-            currentSimIndex: -1
+            currentSimIndex: -1,
         }
     }
 
@@ -21,17 +24,22 @@ class SimulationWrapper extends React.Component {
             console.log(data);
             this.state.sims = data;
 
+
         });
     }
 
     render() {
+        let list = [1,2,3,3,3,3,3];
+
         return (
             <Grid container spacing={12}>
                 <Grid item xs={3}>
-                
+                    {list.map(cur => {
+                        return (<div>hello</div>)
+                    })}
                 </Grid>
 
-                <Grid item xs={3}>
+                <Grid item xs={9}>
                 wasm
                 </Grid>
 
